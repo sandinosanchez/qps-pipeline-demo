d5c79daf620bd1bc91348704231f47ff673782b2
sandinosanchez <sandino320@gmail.com>
2020-06-23 12:30:54 -0300

    more testing

--- src/com/demosoft/jenkins/pipeline/runner/maven/CarinaDemoRunner.groovy
+++ src/com/demosoft/jenkins/pipeline/runner/maven/CarinaDemoRunner.groovy
@@ -13,16 +13,27 @@ class CarinaDemoRunner extends TestNG {
 		pipelineLibrary = 'QPS-Pipeline-demo'
 		runnerClass = 'com.demosoft.jenkins.pipeline.runner.maven.CarinaDenmoRunner'
 		logger.info('CarinaDemoRunner')
+		prepare()
 		super.onPush()
 	}
 
 	@Override
 	public void registerObject(name, object) {
 		if (overriddenFactories.containsKey(object.clazz)) {
-            context.println("overriding ${object.clazz} by ${overriddenFactories.get(object.clazz)}")
-            object.setClass(overriddenFactories.get(object.clazz))
-        }
-        super.registerObject(name, object)
+			context.println("overriding ${object.clazz} by ${overriddenFactories.get(object.clazz)}")
+			object.setClass(overriddenFactories.get(object.clazz))
+		}
+		super.registerObject(name, object)
+	}
+
+	protected void prepare() {
+		context.node("master") {
+			scmClient.clone(false)
+			scmClient.clone(context.env.getEnvironment().get("CARINA_DEMO_PIPELINE_GIT_URL"), context.env.getEnvironment().get("CARINA_DEMO_GIT_BRANCH"), "qps-demo-pipeline")
+			def additionalClasspath = "qps-pipeline/src" + "\n" + "qps-demo-pipeline/src"
+			context.println("additionalClasspath: " + additionalClasspath)
+			setDslClasspath(additionalClasspath)
+		}
 	}
 
 }
