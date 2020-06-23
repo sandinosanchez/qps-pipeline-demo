74d97c9e187e3ae64a3e5af92ffb9b0650a06bcb
sandinosanchez <sandino320@gmail.com>
2020-06-23 11:17:06 -0300

    overriden factories test

---
+++ src/com/demosoft/jenkins/jobdsl/factory/pipeline/view/DemoListViewFactory.groovy
@@ -0,0 +1,21 @@
+package com.demosoft.jenkins.jobdsl.factory.pipeline.view
+
+import groovy.transform.InheritConstructors
+import com.qaprosoft.jenkins.jobdsl.factory.pipeline.CronJobFactory
+
+@InheritConstructors
+class DemoListViewFactory extends ListViewFactory {
+
+	@Override
+	def create() {
+		def view = super.create()
+
+		view.with {
+			colums {
+				progressBar()
+				scmType()
+			}
+		}
+	}
+
+}
--- src/com/demosoft/jenkins/pipeline/runner/maven/CarinaDemoRunner.groovy
+++ src/com/demosoft/jenkins/pipeline/runner/maven/CarinaDemoRunner.groovy
@@ -1,18 +1,12 @@
 package com.demosoft.jenkins.pipeline.runner.maven
 
+import groovy.transform.InheritConstructors
 import com.qaprosoft.jenkins.pipeline.runner.maven.TestNG
 
+@InheritConstructors
 class CarinaDemoRunner extends TestNG {
 
-	// overriden factories
-
-	CarinaDemoRunner(context) {
-		super(context)
-	}
-
-	CarinaDemoRunner(context, jobType) {
-		super(context, jobType)
-	}
+	def overridenFactories = ['com.qaprosoft.jenkins.jobdsl.factory.pipeline.view.ListViewFactory' : 'com.demosoft.jenkins.jobdsl.factory.pipeline.view.DemoListViewFactory']
 
 	@Override
 	public void onPush() {
@@ -22,13 +16,13 @@ class CarinaDemoRunner extends TestNG {
 		super.onPush()
 	}
 
-	protected void prepare() {
-        context.node("master") {
-            scmClient.clone(false)
-            scmClient.clone(context.env.getEnvironment().get("CARINA_DEMO_PIPELINE_URL"), context.env.getEnvironment().get("CARINA_DEMO_PIPELINE_BRANCH"), "qps-demo-pipeline")
-            def additionalClasspath = "qps-pipeline/src" + "\n" + "qps-demo-pipeline/src"
-            context.println("additionalClasspath: " + additionalClasspath)
-            setDslClasspath(additionalClasspath)
+	@Override
+	public void registerObject(name, object) {
+		if (overriddenFactories.containsKey(object.clazz)) {
+            context.println("overriding ${object.clazz} by ${overriddenFactories.get(object.clazz)}")
+            object.setClass(overriddenFactories.get(object.clazz))
         }
-    }
+        super.registerObject(name, object)
+	}
+
 }
