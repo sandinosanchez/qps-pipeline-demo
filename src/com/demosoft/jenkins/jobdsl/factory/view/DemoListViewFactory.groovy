2614e29b2a04c8f210a2c952026be23a2a0e7170
sandinosanchez <sandino320@gmail.com>
2020-06-25 06:46:13 -0300

    even more testing

--- src/com/demosoft/jenkins/jobdsl/factory/view/DemoListViewFactory.groovy
+++ src/com/demosoft/jenkins/jobdsl/factory/view/DemoListViewFactory.groovy
@@ -1,21 +1,64 @@
 package com.demosoft.jenkins.jobdsl.factory.view
 
 import groovy.transform.InheritConstructors
-import com.qaprosoft.jenkins.jobdsl.factory.view.ListViewFactory
+import com.qaprosoft.jenkins.jobdsl.factory.DslFactory
 
 @InheritConstructors
-class DemoListViewFactory extends ListViewFactory {
+class DemoListViewFactory extends DslFactory {
 
-	@Override
-	def create() {
-		def view = super.create()
+	def folder
+    def name
+    def descFilter
+    def nameFilter
 
-		view.with {
-			columns {
-				progressBar()
-				scmType()
-			}
-		}
-	}
+    public DemoListViewFactory(folder, name, descFilter) {
+        this(folder, name, descFilter, "")
+    }
+    
+    public DemoListViewFactory(folder, name, descFilter, nameFilter) {
+        this.folder = folder
+        this.name = name
+        this.descFilter = descFilter
+        this.nameFilter = nameFilter
+    }
 
+    def create() {
+        //TODO: reuse getFullName
+        def view = _dslFactory.listView("${folder}/${name}")
+        view.with {
+            columns {
+                status()
+                weather()
+                name()
+                lastSuccess()
+                lastFailure()
+                lastDuration()
+                progrsBar()
+                scmType()
+                buildButton()
+            }
+
+            //TODO: reorganize constructor to be able to provide RegexMatchValue
+            if (!"${descFilter}".isEmpty()) {
+                jobFilters {
+                    regex {
+                        matchType(MatchType.INCLUDE_MATCHED)
+                        matchValue(RegexMatchValue.DESCRIPTION)
+                        regex("${descFilter}")
+                    }
+                }
+            }
+
+            if (!"${nameFilter}".isEmpty()) {
+                jobFilters {
+                    regex {
+                        matchType(MatchType.INCLUDE_MATCHED)
+                        matchValue(RegexMatchValue.NAME)
+                        regex("${nameFilter}")
+                    }
+                }
+            }
+        }
+        return view
+    }
 }
