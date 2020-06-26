fe0430a489b7aabecd82bf8c47cd3c7f40ac167a
sandinosanchez <sandino320@gmail.com>
2020-06-26 14:59:37 -0300

    final fixes

--- src/com/qaprosoft/jenkins/pipeline/runner/maven/CarinaDemo.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/maven/CarinaDemo.groovy
@@ -1,4 +1,4 @@
-package com.demosoft.jenkins.pipeline.runner.maven
+package com.qaprosoft.jenkins.pipeline.runner.maven
 
 import groovy.transform.InheritConstructors
 import com.qaprosoft.jenkins.pipeline.runner.maven.TestNG
@@ -6,12 +6,12 @@ import com.qaprosoft.jenkins.pipeline.runner.maven.TestNG
 @InheritConstructors
 class CarinaDemo extends TestNG {
 
-	def overriddenFactories = ['com.qaprosoft.jenkins.jobdsl.factory.pipeline.TestJobFactory' : 'com.demosoft.jenkins.jobdsl.factory.pipeline.DemoTestJobFactory']
+	def overriddenFactories = ['com.qaprosoft.jenkins.jobdsl.factory.pipeline.TestJobFactory' : 'com.qaprosoft.jenkins.jobdsl.factory.pipeline.DemoTestJobFactory']
 
 	@Override
 	public void onPush() {
 		pipelineLibrary = 'QPS-Pipeline-demo'
-		runnerClass = 'com.demosoft.jenkins.pipeline.runner.maven.CarinaDemo'
+		runnerClass = 'com.qaprosoft.jenkins.pipeline.runner.maven.CarinaDemo'
 		prepare()
 		super.onPush()
 	}
@@ -28,8 +28,8 @@ class CarinaDemo extends TestNG {
 	protected void prepare() {
 		context.node("master") {
 			scmClient.clone(false)
-            scmClient.clone(configuration.getGlobalProperty("CARINA_DEMO_PIPELINE_URL"), configuration.getGlobalProperty("CARINA_DEMO_PIPELINE_BRANCH"), "qps-demo-pipeline")
-			def additionalClasspath = "qps-pipeline/src" + "\n" + "qps-demo-pipeline/src"
+            scmClient.clone(configuration.getGlobalProperty("CARINA_DEMO_PIPELINE_GIT_URL"), configuration.getGlobalProperty("CARINA_DEMO_PIPELINE_GIT_BRANCH"), "carina-demo-pipeline")
+			def additionalClasspath = "qps-pipeline/src" + "\n" + "carina-demo-pipeline/src"
 			context.println("additionalClasspath: " + additionalClasspath)
 			setDslClasspath(additionalClasspath)
 		}
